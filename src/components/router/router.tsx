6765de19dc119ca6cf247310e38902dd354cce7d
Vladyslav Dmytrenko <vlad.dmitrenko@itechcraft.com>
2023-09-04 15:41:56 +0300

    added deploy config

--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -1,53 +1,56 @@
 import { ROUTER_URL_LIST } from '@/constant';
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 
-export const router = createBrowserRouter([
-  {
-    path: ROUTER_URL_LIST.PRODUCT,
-    async lazy() {
-      const { Product } = await import('@/pages/Product');
+export const router = createBrowserRouter(
+  [
+    {
+      path: ROUTER_URL_LIST.PRODUCT,
+      async lazy() {
+        const { Product } = await import('@/pages/Product');
 
-      return { element: <Product /> };
+        return { element: <Product /> };
+      },
+      children: [
+        {
+          index: true,
+          element: <Navigate to={ROUTER_URL_LIST.PRODUCT_OVERVIEW} />,
+        },
+        {
+          path: ROUTER_URL_LIST.PRODUCT_OVERVIEW,
+          async lazy() {
+            const { ProductOverview } = await import(
+              '@pages/Product/ProductOverview'
+            );
+            return {
+              element: <ProductOverview />,
+            };
+          },
+        },
+        {
+          path: ROUTER_URL_LIST.PRODUCT_CREATE,
+          async lazy() {
+            const { ProductCreate } = await import(
+              '@pages/Product/ProductCreate'
+            );
+            return {
+              element: <ProductCreate />,
+            };
+          },
+        },
+
+        {
+          path: ROUTER_URL_LIST.PRODUCT_DETAILS,
+          async lazy() {
+            const { ProductDetail } = await import(
+              '@pages/Product/ProductDetail'
+            );
+            return {
+              element: <ProductDetail />,
+            };
+          },
+        },
+      ],
     },
-    children: [
-      {
-        index: true,
-        element: <Navigate to={ROUTER_URL_LIST.PRODUCT_OVERVIEW} />,
-      },
-      {
-        path: ROUTER_URL_LIST.PRODUCT_OVERVIEW,
-        async lazy() {
-          const { ProductOverview } = await import(
-            '@pages/Product/ProductOverview'
-          );
-          return {
-            element: <ProductOverview />,
-          };
-        },
-      },
-      {
-        path: ROUTER_URL_LIST.PRODUCT_CREATE,
-        async lazy() {
-          const { ProductCreate } = await import(
-            '@pages/Product/ProductCreate'
-          );
-          return {
-            element: <ProductCreate />,
-          };
-        },
-      },
-
-      {
-        path: ROUTER_URL_LIST.PRODUCT_DETAILS,
-        async lazy() {
-          const { ProductDetail } = await import(
-            '@pages/Product/ProductDetail'
-          );
-          return {
-            element: <ProductDetail />,
-          };
-        },
-      },
-    ],
-  },
-]);
+  ],
+  { basename: import.meta.env.DEV ? '/' : '/store-test-task/' }
+);
